8a448aeaa053ef034e72311137020d985be4b6be
Ketty <yuriy.sev51@gmail.com>
2023-01-27 17:18:24 +0200

    added open sourced link

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This app was coded by Ketty and is{" "}
-        {/* <a
+        <a
           href="https://github.com/KettySalt/react-matt-weather"
           target="_blank"
           rel="noreferrer"
         >
           open sourced
         </a>{" "}
-        and hosted on{" "}
+        {/* and hosted on{" "}
         <a
           href="https://github.com/KettySalt/react-matt-weather"
           target="_blank"
           rel="noreferrer"
         >
-          Netlify
-        </a> */}
+          Netlify */}
+        {/* </a> */}
       </footer>
     </div>
   );
